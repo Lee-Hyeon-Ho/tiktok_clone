34aa9e53354bfa42b079fcb5e02d8f9ae33b487f
이현호 <35526198+Lee-Hyeon-Ho@users.noreply.github.com>
2023-02-20 00:22:23 +0900

    tiktok clone login screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+
+void main() {
+  runApp(const TicTokApp());
+}
+
+class TicTokApp extends StatelessWidget {
+  const TicTokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TicTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: const SignUpScreen(),
+    );
+  }
+}
